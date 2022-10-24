ac888d9bc8ead16429c27f74eba258a48f7b684e
matolgithub <89175955650@mail.ru>
2022-10-24 15:24:41 +0300

    First commit.

---
+++ main.py
@@ -0,0 +1,19 @@
+from PIL import Image
+from pathlib import Path
+from rembg import remove
+
+
+def change_jpg_png():
+    pass
+
+
+def png_without_bg():
+    pass
+
+
+def main():
+    png_without_bg()
+
+
+if __name__ == "__main__":
+    main()
